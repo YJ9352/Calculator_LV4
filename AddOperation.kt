2cc7dcd7b57691059d30d5ed6dfdbe8a864ca892
YJ9352 <150547656+YJ9352@users.noreply.github.com>
2023-12-07 10:59:21 +0900

    open 삭제

--- AddOperation.kt
+++ AddOperation.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication.calculator;
 
-open class AddOperation: AbstractOperation() {
+class AddOperation: AbstractOperation() {
     override fun operate(num1: Int, num2: Int): Double {
         return (num1 + num2).toDouble()
     }
